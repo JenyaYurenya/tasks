e37c32f1305a8f2515dc31ae9dbf5aa143375c8d
Jenya <caxarok56@gmail.com>
2022-02-09 17:24:02 +0300

    09/02

---
+++ number_0/15/app.js
@@ -0,0 +1,4 @@
+const n = +prompt ("Число");
+Math.round((n+0)/2)
+console.log("Количество выживших",Math.round((n+0)/2))
+
---
+++ number_0/16/app.js
@@ -0,0 +1,4 @@
+const n = +prompt ("Номер места");
+Math.round((n+3)/4)
+console.log("Номер купе",Math.round((n+3)/4))
+
